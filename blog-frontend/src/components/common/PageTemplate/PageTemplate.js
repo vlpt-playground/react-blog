bbbee29f1a2d9f8f5ca814b167763fe61a73dc23
velopert <public.velopert@gmail.com>
2018-02-03 07:09:44 +0900

    Header, Footer 컴포넌트 만들기

--- blog-frontend/src/components/common/PageTemplate/PageTemplate.js
+++ blog-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
+import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
 
 const cx = classNames.bind(styles);
 
 const PageTemplate = () => (
   <div className={cx('page-template')}>
-    PageTemplate
+    <Header/>
+    <Footer/>
   </div>
 );
 
