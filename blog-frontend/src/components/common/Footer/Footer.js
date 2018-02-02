e46d21bc6957a4ffe08f85b84f9f20ec6b5b79fb
velopert <public.velopert@gmail.com>
2018-02-03 06:52:46 +0900

    컴포넌트 만들기

---
+++ blog-frontend/src/components/common/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Footer.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const Footer = () => (
+  <div>
+    Footer
+  </div>
+);
+
+export default Footer;
