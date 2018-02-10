6f8c98322567b78bdafafc63f902f26c3223e200
velopert <public.velopert@gmail.com>
2018-02-10 10:04:29 +0900

    포스트 수정 및 삭제하기

---
+++ blog-frontend/src/containers/common/HeaderContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Header from 'components/common/Header';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as baseActions from 'store/modules/base';
+
+class HeaderContainer extends Component {
+  handleRemove = () => {
+    const { BaseActions } = this.props;
+    BaseActions.showModal('remove');
+  }
+
+  render() {
+    const { handleRemove } = this;
+    const { match } = this.props;
+
+    const { id } = match.params;
+
+    return (
+      <Header
+        postId={id}
+        onRemove={handleRemove}
+      />
+    );
+  }
+}
+
+export default connect(
+  (state) => ({}),
+  (dispatch) => ({
+    BaseActions: bindActionCreators(baseActions, dispatch)
+  })
+)(withRouter(HeaderContainer));
--- blog-frontend/src/store/modules/editor.js
+++ blog-frontend/src/store/modules/editor.js
@@ -9,12 +9,15 @@ import * as api from 'lib/api';
 const INITIALIZE = 'editor/INITIALIZE';
 const CHANGE_INPUT = 'editor/CHANGE_INPUT';
 const WRITE_POST = 'editor/WRITE_POST';
+const GET_POST = 'editor/GET_POST';
+const EDIT_POST = 'editor/EDIT_POST';
 
 // action creators
 export const initialize = createAction(INITIALIZE);
 export const changeInput = createAction(CHANGE_INPUT);
 export const writePost = createAction(WRITE_POST, api.writePost);
-
+export const getPost = createAction(GET_POST, api.getPost);
+export const editPost = createAction(EDIT_POST, api.editPost);
 
 // initial state
 const initialState = Map({
@@ -23,6 +26,7 @@ const initialState = Map({
   tags: '',
   postId: null
 });
+
 
 // reducer
 export default handleActions({
@@ -36,6 +40,15 @@ export default handleActions({
     onSuccess: (state, action) => {
       const { _id } = action.payload.data;
       return state.set('postId', _id);
+    }
+  }),
+  ...pender({
+    type: GET_POST,
+    onSuccess: (state, action) => {
+      const { title, tags, body } = action.payload.data;
+      return state.set('title', title)
+                  .set('markdown', body)
+                  .set('tags', tags.join(', ')); // 배열 -> ,로 구분된 문자열
     }
   })
 }, initialState)
